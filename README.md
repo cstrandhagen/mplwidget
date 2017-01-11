6a980bae6054c6c4bb084075669eb352581ce592
Christian Strandhagen <strandhagen@pit.physik.uni-tuebingen.de>
2017-01-11 11:41:50 +0100

    Switch to setuptools, add README.

---
+++ README.md
@@ -0,0 +1,5 @@
+MPLWidget - A Matplotlib Widget for QT
+======================================
+
+Provides a Widget for use in PyQt Applications with additional features
+like fitting (uses lmfit) and editing graph properties.
--- setup.py
+++ setup.py
@@ -1,30 +1,113 @@
-#!/usr/bin/env python
 '''
 Created on Aug 1, 2013
 
 @author: strandha
 '''
 
+from setuptools import setup, find_packages
 
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration(None, parent_package, top_path)
-    config.set_options(ignore_setup_xxx_py=True,
-                       assume_default_configuration=True,
-                       delegate_options_to_subpackages=True,
-                       quiet=True)
+from codecs import open
+from os import path
 
-    config.add_subpackage('mplwidget')
+here = path.abspath(path.dirname(__file__))
 
-    return config
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-if __name__ == '__main__':
-    from numpy.distutils.core import setup
+setup(
+    name='mplwidget',
 
-    setup(version='0.1.0',
-          description='mplwidget - A Matplotlib Widget',
-          author='Christian Strandhagen',
-          author_email='strandhagen@pit.physik.uni-tuebingen.de',
-          license='no idea',
-          install_requires=['matplotlib','lmfit','PyQt4'],
-          **configuration(top_path='').todict())
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.0',
+
+    description='Matplotlib Widget with enhanced features',
+    long_description=long_description,
+
+    # The project's main homepage.
+    # url='https://github.com/pypa/sampleproject',
+
+    # Author details
+    author='Christian Strandhagen',
+    author_email='strandhagen@pit.physik.uni-tuebingen.de',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Visualization',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='matplotlib plotting fitting',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'build',
+                                    'dist', 'mplwidget.egg-info']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['matplotlib', 'lmfit'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+        'icons': ['icons/*.png'],
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
+)
